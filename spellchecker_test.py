bd6a7e64a360d237166949178d57fa54d8b4e259
Cbowman15 <bowman.c@northeastern.edu>
2023-12-10 11:12:01 -0500

    finished up some tests

--- spellchecker_test.py
+++ spellchecker_test.py
@@ -10,16 +10,80 @@ personal_dict_file_path = "C:\\Users\\cb6f1\\OneDrive\\Desktop\\Project\\spellch
 ignored_words_file_path = "C:\\Users\\cb6f1\\OneDrive\\Desktop\\Project\\spellchecker\\ign_words.txt"
 
 class TestSpellchecker(unittest.TestCase):
-    def fxn():
-        return
+    def test_spell_check_unknown(self):
+        class MockReferenceFile:
+            def parse(self):
+                return ["Holly", "Jlly", "christmas."]
+        reference_file = MockReferenceFile()
+        known_words_file_path = "known_words.txt"
+        personal_dict_file_path = "pers_dict.txt"
+        ignored_words_file_path = "ign_words.txt"
+        spellchecker = Spellchecker(reference_file, known_words_file_path,personal_dict_file_path,ignored_words_file_path)
+        spellchecker.spell_check()
+        self.assertEqual(spellchecker.unknown_words, [("Jlly", [])])
     
+    def test_spell_check_known(self):
+        class MockReferenceFile:
+            def parse(self):
+                return ["Holly", "Jolly", "christmas."]
+        reference_file = MockReferenceFile()
+        known_words_file_path = "known_words.txt"
+        personal_dict_file_path = "pers_dict.txt"
+        ignored_words_file_path = "ign_words.txt"
+        spellchecker = Spellchecker(reference_file, known_words_file_path, personal_dict_file_path,ignored_words_file_path)
+        spellchecker.known_words = {"Holly", "Jolly", "christmas."}
+        spellchecker.spell_check()
+        self.assertNotIn(spellchecker.unknown_words, [("Holly", []), ("Jolly", []), ("christmas.", [])])
+
+    def test_start_sentence(self):
+        words_to_check = ["Have", "a", "holly", ",", "jolly,", "Christmas", "."]
+        spellchecker = Spellchecker(reference_file, known_words_file_path,personal_dict_file_path,ignored_words_file_path)
+        check = spellchecker.start_sentence(-1, words_to_check)
+        self.assertFalse(check)
+
+    def test_get_known_words(self):
+        known_words_file_path = "known_words.txt"
+        personal_dict_file_path = "pers_dict.txt"
+        ignored_words_file_path = "ign_words.txt"
+        spellchecker = Spellchecker(None, known_words_file_path,personal_dict_file_path,ignored_words_file_path)
+        known_words = spellchecker.get_known_words(known_words_file_path,personal_dict_file_path)
+        self.assertIsInstance(known_words, set)
+    
+class TestReferenceFile(unittest.TestCase):
+    def test_text_parse(self):
+        text = "It's the best time of the year."
+        reference_file = TextFile(text)
+        words = reference_file.parse()
+        self.assertEqual(words, ["It's", "the", "best", "time", "of", "the", "year."])
+    
+    def test_html_parse(self):
+        text = "<html><body><p>Say hello to friends you know</p></body></html>"
+        reference_file = HTMLFile(text)
+        words = reference_file.parse()
+        self.assertEqual(words, ["Say", "hello", "to", "friends", "you", "know"])
+
+    def test_docx_parse(self):
+        text = "and everyone you meet."
+        docx_file = DocxFile(text)
+        words = ["and", "everyone", "you", "meet."]
+        self.assertEqual(docx_file.parse(), words)
+
+class TestSuggester(unittest.TestCase):
+    def test_get_suggestions(self):
+        known_words = {"tech", "ten", "the"}
+        unknown = "teh"
+        pos_sug = sorted(["the", "tech", "ten"])
+        suggester = Suggester(known_words)
+        suggestions = suggester.get_suggestions(unknown, known_words)
+        self.assertEqual(suggestions, pos_sug)
+
 class TestSpellcheckerApp(unittest.TestCase):
     def test_refresh(self):
         window = tk.Tk()
         reference_file = type("ReferenceFile", (), {"text": ""})()
         known_words_file_path = "known_words.txt"
-        personal_dict_file_path = "personal_dict.txt"
-        ignored_words_file_path = "ignored_words.txt"
+        personal_dict_file_path = "pers_dict.txt"
+        ignored_words_file_path = "ign_words.txt"
         spellchecker = Spellchecker(reference_file, known_words_file_path, personal_dict_file_path, ignored_words_file_path)
         spellcheckerapp = SpellcheckerApp(window, spellchecker)
 
